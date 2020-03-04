81d0a2c795ec05f8df8eb45769db07a0e4c50bfd
Greyflanel <richardlovelace811@gmail.com>
2020-03-03 19:15:39 -0600

    still working on how to handle the merged_arr=[]*elements

--- src/mine.py
+++ src/mine.py
@@ -1,12 +1,33 @@
-arr = [6, 5, 8, 4, 2, 9, 1, 0, 3, 7]
 
- 
-def bubble_sort( arr ):
-    sort_complete = len(arr)
-    while sort_complete != 0:
-        for i in range(len(arr) - 1):
-            if arr[i] > arr[i + 1]:
-                arr[i], arr[i + 1] = arr[i + 1], arr[i]
-        sort_complete -= 1        
-    return print(arr)
-bubble_sort(arr)
+arrA = [6, 7, 8, 9]
+arrB = [1, 2, 3, 4, 5]
+
+
+def merge(arrA, arrB):
+    elements = len(arrA) + len(arrB)
+    merged_arr = [0] * elements
+    i = 0
+    j = 0
+    
+    while i < len(arrA) and j < len(arrB):
+        if(arrA[i] <= arrB[j]):
+            merged_arr.append(arrA[i])
+            i+=1
+        else:
+            merged_arr.append(arrB[j])
+            j+=1
+    return merged_arr + arrA[i:] + arrB[j:]
+
+merge(arrA, arrB)
+
+def merge_sort( arr ):
+    if(len(arr) <= 1):
+        return arr
+    half = len(arr) // 2
+    left = merge_sort(arr[0:half])
+    right = merge_sort(arr[half:])
+    
+    return merge(left, right)
+
+print(merge_sort([8,5,3,7,9]))
+    
--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,33 +1,64 @@
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
-def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
+def merge(arrA, arrB):
+    elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
-    # TO-DO
+    i = 0
+    j = 0
+
+    while i < len(arrA) and j < len(arrB):
+        if(arrA[i] <= arrB[j]):
+            merged_arr.append(arrA[i])
+            i+=1
+        else:
+            merged_arr.append(arrB[j])
+            j+=1
+    return merged_arr + arrA[i:] + arrB[j:]
+
+
+
+def merge(arrA, arrB):
+    elements = len(arrA) + len(arrB)
+    merged_arr = [0] * elements
+    merged_arr = []
+    i = 0
+    j = 0
     
-    return merged_arr
+    while i < len(arrA) and j < len(arrB):
+        if(arrA[i] <= arrB[j]):
+            merged_arr.append(arrA[i])
+            i+=1
+        else:
+            merged_arr.append(arrB[j])
+            j+=1
+    return merged_arr + arrA[i:] + arrB[j:]
 
 
-# TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
-    # TO-DO
+    if(len(arr) <= 1):
+        return arr
+    half = len(arr) // 2
+    left = merge_sort(arr[0:half])
+    right = merge_sort(arr[half:])
+    
+    return merge(left, right)
 
-    return arr
-
-
-# STRETCH: implement an in-place merge sort algorithm
-def merge_in_place(arr, start, mid, end):
-    # TO-DO
-
-    return arr
-
-def merge_sort_in_place(arr, l, r): 
-    # TO-DO
-
-    return arr
+print(merge_sort([8,5,3,7,9]))
 
 
-# STRETCH: implement the Timsort function below
-# hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
-def timsort( arr ):
+# # STRETCH: implement an in-place merge sort algorithm
+# def merge_in_place(arr, start, mid, end):
+#     # TO-DO
 
-    return arr
+#     return arr
+
+# def merge_sort_in_place(arr, l, r):
+#     # TO-DO
+
+#     return arr
+
+
+# # STRETCH: implement the Timsort function below
+# # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
+# def timsort( arr ):
+
+#     return arr
